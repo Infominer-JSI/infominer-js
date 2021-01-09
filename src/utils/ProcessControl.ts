4dbc053342dd0e511176550ce495ce9527e60cd8
eriknovak <novak.erik@gmail.com>
2021-01-09 15:43:59 +0100

    Minor changes

--- src/utils/ProcessControl.ts
+++ src/utils/ProcessControl.ts
@@ -152,12 +152,10 @@ export default class ProcessControl {
         return new Promise((resolve, reject) => {
             console.log("Running stop process tasks for child id=", childId);
             try {
-                console.log("Send request");
                 this.sendAndWait(childId, { cmd: EParentCmd.SHUTDOWN }, (error) =>
                     error ? reject(error) : resolve(null)
                 );
             } catch (xerror) {
-                console.log("Child already closed");
                 return reject(xerror);
             }
         });
